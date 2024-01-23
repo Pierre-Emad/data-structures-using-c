af9154e7bfc7bde407a01df84c147e987edc80b5
Pierre Emad <68783676+Pierre-Emad@users.noreply.github.com>
2024-01-23 16:44:32 +0200

    Updated source file
    More functions, added status codes

--- Singly Linked List/LINKEDLIST_S.c
+++ Singly Linked List/LINKEDLIST_S.c
@@ -8,8 +8,9 @@
 #include <stdlib.h>
 #include "LINKEDLIST_S.h"
 
-void linkedlist_insertAtStart(node **linked_list_head, uint32_t data)
+LINKEDL_STATUS_t linkedlist_insertAtStart(node **linked_list_head, uint32_t data)
 {
+    LINKEDL_STATUS_t ret_status = LINKEDLIST_NOK;
     node *newNode = NULL;
     newNode = (node *) malloc(sizeof(node));
     if (newNode != NULL)
@@ -22,12 +23,14 @@ void linkedlist_insertAtStart(node **linked_list_head, uint32_t data)
             /* if the list was empty, the new node points to null */
             newNode->LINKEDLIST_next_node = NULL;
             *linked_list_head = newNode;
+            ret_status = LINKEDLIST_EMPTY;
         }
         else
         {
             /* if it wasn't empty, the new node points to the old head */
             newNode->LINKEDLIST_next_node = *linked_list_head;
             *linked_list_head = newNode;
+            ret_status = LINKEDLIST_OK;
         }
     }
     else
@@ -36,10 +39,12 @@ void linkedlist_insertAtStart(node **linked_list_head, uint32_t data)
         printf("Failed to allocate space for new node\n");
         #endif
     }
+    return ret_status;
 }
 
-void linkedlist_insertAtEnd(node **linked_list_head, uint32_t data)
+LINKEDL_STATUS_t linkedlist_insertAtEnd(node **linked_list_head, uint32_t data)
 {
+    LINKEDL_STATUS_t ret_status = LINKEDLIST_NOK;
     node* newNode = NULL;
     node* lastNode = NULL; // will be needed in case of non-empty list
     newNode = (node *) malloc(sizeof(node));
@@ -52,6 +57,7 @@ void linkedlist_insertAtEnd(node **linked_list_head, uint32_t data)
         if (*linked_list_head == NULL)
         {
             *linked_list_head = newNode;
+            ret_status = LINKEDLIST_EMPTY;
         }
         else
         {
@@ -63,6 +69,7 @@ void linkedlist_insertAtEnd(node **linked_list_head, uint32_t data)
             }
             /* the new node will be after the last node in the list */
             lastNode->LINKEDLIST_next_node = newNode;
+            ret_status = LINKEDLIST_OK;
         }
     }
     else
@@ -71,14 +78,16 @@ void linkedlist_insertAtEnd(node **linked_list_head, uint32_t data)
         printf("Failed to allocate space for new node\n");
         #endif
     }
+    return ret_status;
 }
 
-void linkedlist_insertAt(node *linked_list_head, uint32_t data, uint16_t insertPosition)
+LINKEDL_STATUS_t linkedlist_insertAt(node *linked_list_head, uint32_t data, uint16_t insertPosition)
 {
+    LINKEDL_STATUS_t ret_status = LINKEDLIST_NOK;
     node *newNode = NULL, *prevNode = NULL;
     uint16_t currentPosition = 1, listLength = 0;
 
-    listLength = linkedlist_getLength(linked_list_head);
+    listLength = linkedlist_getLength(linked_list_head, NULL);
 
     if (insertPosition == 1)
     {
@@ -95,6 +104,7 @@ void linkedlist_insertAt(node *linked_list_head, uint32_t data, uint16_t insertP
         newNode = (node *) malloc(sizeof(node));
         if (newNode == NULL)
         {
+            ret_status = LINKEDLIST_NOK;
             #ifdef LINKEDLIST_DEBUG_MODE
             printf("Failed to allocate space for new node\n");
             #endif
@@ -111,36 +121,101 @@ void linkedlist_insertAt(node *linked_list_head, uint32_t data, uint16_t insertP
             /* connect the new node to the next node (leftside connection) */
             newNode->LINKEDLIST_next_node = prevNode->LINKEDLIST_next_node;
             /* connect the previous node to the new node (rightside connection) */
-            prevNode->LINKEDLIST_next_node = newNode; 
+            prevNode->LINKEDLIST_next_node = newNode;
+            ret_status = LINKEDLIST_OK; 
         }
     }
+    return ret_status;
 }
 
-uint16_t linkedlist_getLength(node *linked_list_head)
+LINKEDL_STATUS_t linkedlist_deleteAtStart(node **linked_list_head)
 {
-    node *head = linked_list_head;
-    uint16_t linked_list_length;
-    if (head == NULL)
+    LINKEDL_STATUS_t ret_status = LINKEDLIST_NOK;
+    node* oldHead = *linked_list_head;
+    if (oldHead == NULL)
     {
-        linked_list_length = 0;
+        ret_status = LINKEDLIST_EMPTY;
+        #ifdef LINKEDLIST_DEBUG_MODE
+        printf("List is empty, cannot delete\n");
+        #endif
     }
     else
     {
-        linked_list_length = 0;
-        while (head != NULL)
+        *linked_list_head = oldHead->LINKEDLIST_next_node;
+        oldHead->LINKEDLIST_next_node = NULL; // to avoid dangling pointers
+        free(oldHead);
+        ret_status = LINKEDLIST_OK;
+    }
+    return ret_status;
+}
+
+LINKEDL_STATUS_t linkedlist_deleteAt(node *linked_list_head, uint16_t deletePosition)
+{
+    LINKEDL_STATUS_t ret_status = LINKEDLIST_NOK;
+    node *oldNode = NULL, *prevNode = NULL;
+    uint16_t currentPosition = 1, listLength = 0;
+
+    listLength = linkedlist_getLength(linked_list_head, NULL);
+
+    if (deletePosition == 1)
+    {
+        printf("Please use linkedlist_deleteAtStart() function\n");
+    }
+    else if (deletePosition > listLength)
+    {
+        #ifdef LINKEDLIST_DEBUG_MODE
+        printf("Deletion Position is greater than the list size\n");
+        #endif
+    }
+    else
+    {
+        prevNode = linked_list_head;
+        while (currentPosition < deletePosition - 1)
         {
-            linked_list_length++;
-            head = head->LINKEDLIST_next_node;
+            prevNode = prevNode->LINKEDLIST_next_node;
+            currentPosition++;
+        }
+        oldNode = prevNode->LINKEDLIST_next_node;
+        prevNode->LINKEDLIST_next_node = oldNode->LINKEDLIST_next_node;
+        oldNode->LINKEDLIST_next_node = NULL;
+        free(oldNode);
+        ret_status = LINKEDLIST_OK;
+    }
+    return ret_status;
+}
+
+uint16_t linkedlist_getLength(node *linked_list_head, LINKEDL_STATUS_t *status)
+{
+    node *head = linked_list_head;
+    uint16_t linked_list_length;
+    linked_list_length = 0;
+    while (head != NULL)
+    {
+        linked_list_length++;
+        head = head->LINKEDLIST_next_node;
+    }
+    if (status != NULL)
+    {
+        if (linked_list_length == 0)
+        {
+            *status = LINKEDLIST_EMPTY;
+        }
+        else
+        {
+            *status = LINKEDLIST_OK;
         }
     }
+    else {}
     return linked_list_length;
 }
 
-void linkedlist_print(node *linked_list_head)
+LINKEDL_STATUS_t linkedlist_print(node *linked_list_head)
 {
+    LINKEDL_STATUS_t ret_status = LINKEDLIST_NOK;
     node *head = linked_list_head;
     if (head == NULL)
     {
+        ret_status = LINKEDLIST_EMPTY;
         #ifdef LINKEDLIST_DEBUG_MODE
         printf("Linked list is empty, cannot print\n");
         #endif
@@ -153,5 +228,7 @@ void linkedlist_print(node *linked_list_head)
             head = head->LINKEDLIST_next_node;
         }
         printf("\n");
+        ret_status = LINKEDLIST_OK;
     }
+    return ret_status;
 }
